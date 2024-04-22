90232e108574ea2cb0872321423950894e50a737
Mauri <mauri.monzon91@gmail.com>
2024-04-21 21:09:23 -0300

    Inicio dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+    // var myName="Mauri";
+    // late final myName
+    // String myName="Mauri"
+  var myName="Mauri";
+
+  print("Hola $myName");
+  print("Hola ${myName.toUpperCase()}");
+  print("Hola ${1+1}");
+}
