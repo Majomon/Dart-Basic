71e525bb0c79676aab0f3c079be82b452e30248a
Mauri <mauri.monzon91@gmail.com>
2024-04-21 21:13:55 -0300

    Dynamic

--- 02_variables.dart
+++ 02_variables.dart
@@ -2,15 +2,26 @@ void main(){
   final String pokemon="Ditto";
   final int hp=100;
   final bool isAlive=true;
-        final List<String>abilities=["Impostor"];
+  final List<String>abilities=["Impostor"];
   final sprites= <String>["ditto/front.png","ditto/back.png"];
   
+  
+  //Dynamic porm defecto es null -> dynamic=null
+  dynamic errorMessage="Hola";
+  errorMessage=true;
+  errorMessage=[1,2,3,4];
+  errorMessage={1,2};
+  errorMessage={()=>true};
+  errorMessage=null;
+  
+  
   print("""
-           $pokemon
+    $pokemon
     $hp
     $isAlive
     $abilities
     $sprites
+    $errorMessage
   """
   );
 }
